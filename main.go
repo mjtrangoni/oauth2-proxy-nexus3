fc73da0dbcced590de92c3a466bbcc0402452466
Mario Trangoni <mjtrangoni@gmail.com>
2023-06-16 10:36:30 +0200

    golang: Print build Information
    Signed-off-by: Mario Trangoni <mjtrangoni@gmail.com>

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"oauth2-proxy-nexus3/logger"
 	"oauth2-proxy-nexus3/reverseproxy"
 
+	"runtime/debug"
+
 	env "github.com/caarlos0/env/v8"
 	"go.uber.org/zap"
 )
@@ -13,6 +15,22 @@ import (
 var cfg = config{}
 
 func main() {
+
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok {
+		settings := make(map[string]string)
+		for _, kv := range buildInfo.Settings {
+			settings[kv.Key] = kv.Value
+		}
+		logger.Info("Build Info",
+			zap.String("go", buildInfo.GoVersion),
+			zap.String("path", buildInfo.Path),
+			zap.Any("settings", settings),
+		)
+	} else {
+		logger.Error("Couldn't read Build Info")
+	}
+
 	if err := env.Parse(&cfg); err != nil {
 		logger.Fatal("Failed to parse the configuration", zap.String("error", err.Error()))
 	}
